0495111019b175d79091256bf698728f20397d85
BARATHLIN <112534630+Barathlin@users.noreply.github.com>
2023-10-21 18:29:08 +0530

    video added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import BooksPage from "./Components/BookPage/BooksPage" 
 import BookDetail from "./Components/BookDetails/BookDetails"
-
+import Video from "./Components/Video"
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<BooksPage/>} />
         <Route path="/book/:_id" element={<BookDetail/>} />
+        <Route path="/video" element={<Video/>} />
       </Routes>
     </Router>
   );
---
+++ src/Components/Video.js
@@ -0,0 +1,14 @@
+const Video=()=>{
+
+    return (<div>
+
+<video controls width="640" height="360">
+  <source src="https://your-video-hosting-site.com/your-video-url.mp4" type="video/mp4"/>
+  Your browser does not support the video tag.
+</video>
+
+    </div>)
+
+}
+
+export default Video;
